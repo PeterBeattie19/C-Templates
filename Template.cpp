6ffc11ca07d97b3bef6cd33676a709ce1a428966
Peter Beattie <peterbeattie19@gmail.com>
2018-05-15 23:30:38 +0100

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -6,6 +6,20 @@ typedef long long ll;
 #define endl "\n"
 #define rep(i,a,b) for(int i = a; i<b; i++)
 
+const vector<string> explode(const string& s, const char& c){
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+		if(n != c) buff+=n; else
+		if(n == c && buff != "") { v.push_back(buff); buff = ""; }
+	}
+	if(buff != "") v.push_back(buff);
+	
+	return v;
+}
+
 int main(){
 	ios::sync_with_stdio(0);
 	cin.tie(0);
